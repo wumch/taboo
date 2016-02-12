8944c3b2e39c36e94057699cc81051b84a8f9efe
wumuch <wumuch@gmail.com>
2016-02-12 13:08:12 +0800

    revision

--- tests/bentchmark.sh
+++ tests/bentchmark.sh
@@ -2,8 +2,9 @@
 
 # curl -vvv -d '{"aaa":"AAA"}' 'http://192.168.139.7:1079/fdffsdf/?fdsfdfad=&fdfdf&fd?fdf=fsdfd'
 
-url='http://192.168.139.7:1079/manage/attach'
-data='sign=6970bd0208c684fe6f4fdd373c9d0382&data={"aaa":"AAA","id":10085}'
+host=127.0.0.1
+url="http://${host}:1079/manage/attach"
+data='key=fsdfdsf&sign=6970bd0208c684fe6f4fdd373c9d0382&prefixes=["abcdefg","晕倒"]&item={"aaa":"AAA","id":10086}'
 
 tmpfile=/dev/shm/taboo.post.data
 echo "${data}" > "${tmpfile}"
